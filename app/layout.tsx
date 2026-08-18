9869164869a33e982cab5e0280b563e47e6a3c2b
FatihTheLarper <fatih.mekic@symphony.is>
2026-08-18 16:11:06 +0200

    fix: Fixed styling of sign in button.

--- app/layout.tsx
+++ app/layout.tsx
@@ -50,9 +50,13 @@ export default function RootLayout({ children }: LayoutProps<"/">) {
         >
           <Show when="signed-out">
             <header className="flex justify-center items-center pt-8 p-4 gap-4 h-16">
-              <SignInButton />
+              <SignInButton>
+                <button className="bg-gray-600 dark:bg-gray-700 text-white rounded-full font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 cursor-pointer hover:bg-gray-800 transition-all">
+                  Sign In
+                </button>
+              </SignInButton>
               <SignUpButton>
-                <button className="bg-green-600 dark:bg-green-700 text-white rounded-full font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 cursor-pointer">
+                <button className="bg-green-600 dark:bg-green-700 text-white rounded-full font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 cursor-pointer hover:bg-green-700 dark:hover:bg-green-900">
                   Sign Up
                 </button>
               </SignUpButton>
